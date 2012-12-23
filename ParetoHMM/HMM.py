c7f8f40f70310f085b721b69b38ff1b9a0b0e579
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-12-23 11:51:20 -0500

    Added scoring to CMRF

--- ParetoHMM/HMM.py
+++ ParetoHMM/HMM.py
@@ -99,19 +99,73 @@ class CMRF(object) :
 		""" Converts hmm to CMRF """
 		self.trained = hmm.trained
 		self.length = hmm.length
-		self.emit = hmm.emit # Emission probs (seq,feat)
-		self.trans = hmm.trans # Transition probs
+		self.emitpsi = hmm.emit # Emission probs (seq,feat)
+		self.seqpsi = hmm.trans # Transition probs
 		self.dims = hmm.dims # (latent,emit) dimspace
 		self.featmap = hmm.featmap # Maps features to ids
 		self.seqmap = hmm.seqmap # Maps aa types to ids
-
-	def train(self,traindata) : 
-		""" Train the HMM and set the params of the model"""
-		pass
+		self.seqphi = [np.ones(dim[0]) for dim in hmm.dims]
+		self.featphi =[np.ones(dim[1]) for dim in hmm.dims] 
 
 	def score(self,seq,obs) : 
 		""" Score a given sequence and observation"""
-		pass
+		# Convert seq to ids
+		sids = [seqmap[aa] for seqmap,aa in zip(self.seqmap,seq)]	
+		# convert feature to ids
+		fids = [featmap[f] for featmap,f in zip(self.featmap,obs)]
+
+		ll = 0.0
+		# sum up scores of emission variables
+		for i in xrange(self.length) : 
+			ll += np.log(self.emitpsi[i][sids[i]][fids[i]])
+		
+		# sum up score of latent variables
+		ll += np.log(0.5)
+		for i in xrange(self.length-1) : 
+			ll+= np.log(self.seqpsi[i][sids[i]][sids[i+1]])
+
+		# sum up the scores of the nodes
+		for i in xrange(self.length) : 
+			ll+=  np.log(self.seqphi[i][sids[i]])
+			ll+= np.log(self.featphi[i][fids[i]])
+		return ll
+
+
+	def decode(self,obs)  : 
+		""" Perform max decoding to get the optimal sequence """
+		# convert feature to ids
+		fids = [featmap[f] for featmap,f in zip(self.featmap,obs)]
+		V = []
+		Ptr = []
+		# For the first element
+		V.append(map(lambda(k):np.log(self.emit[0][k][fids[0]])\
+			+np.log(self.initprob[k]),range(self.dims[0][0])))
+
+		# For the rest of the elements
+		for i in range(1,hmm.length) :
+			ptr_k = []; V_k = [];
+			for k in range(self.dims[i][0]) :
+				X = map(lambda(j):np.log(self.trans[i-1][j][k])+V[i-1][j],\
+					range(self.dims[i-1][0]))
+				maxX = max(X)
+				ptr_k.append(X.index(maxX))
+				V_k.append(np.log(self.emit[i][k][fids[i]])+maxX)
+			Ptr.append(ptr_k)
+			V.append(V_k)
+
+		# retreive the optimal sequence
+		seq_max = []
+		seq_max.append(V[-1].index(max(V[-1])))
+		energy_max = V[-1][seq_max[-1]]
+		for ptr_k in reversed(Ptr) :
+			seq_max.append(ptr_k[seq_max[-1]])
+		seq_max.reverse()
+		
+		# map it back to the original sequence
+		decode_seq = "".join([self.seqmap2[i][seqid] \
+			for i,seqid in enumerate(seq_max)])
+	
+		return energy_max,decode_seq
 
 	def decode(self,obs)  : 
 		""" Perform max decoding to get the optimal sequence """
@@ -192,6 +246,14 @@ if __name__ == '__main__' :
 	feat3 = "HHHHLLLLBBBB"
 	score,seq = hmm.decode(feat3)	
 	print("Decoded seq:{} energy:{} feat:{}".format(seq,score,feat3))
+
+
+	print('*'*10+"Working with CMRF"+"*"*10)
+	cmrf = CMRF(hmm)	
+	print("CMRF seq:{} feat:{} score:{}".\
+		format(seq1,feat1,cmrf.score(seq1,feat1)))
+
+
 
 
 
